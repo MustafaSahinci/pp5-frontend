e05b08a42bd008ad88175ddce3d30815d766ab1f
Mustafa <mustafa_sahinci@hotmail.com>
2022-12-12 21:56:31 +0000

    added asset to components and Carcreateform

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import "./api/axiosDefaults";
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
+import CarCreateForm from './pages/cars/CarCreateForm';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <SignInForm /> } />
           <Route exact path="/signup" render={() => <SignUpForm /> } />
+          <Route exact path="/cars/create" render={() => <CarCreateForm /> } />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
