ffb1e7aa47ee256352a855f9389c6e1a980b33a2
Mustafa <mustafa_sahinci@hotmail.com>
2022-12-15 01:36:51 +0000

    add unfollow function

--- src/pages/cars/CarPage.js
+++ src/pages/cars/CarPage.js
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
-import appStyles from "../../App.module.css";
-import { useParams } from "react-router";
-import { axiosReq } from "../../api/axiosDefaults";
-import Car from "./Car";
+import appStyles from '../../App.module.css';
+import { useParams } from 'react-router';
+import { axiosReq } from '../../api/axiosDefaults';
+import Car from './Car';
+import PopularProfiles from '../profiles/PopularProfiles';
 
 function CarPage() {
   const { id } = useParams();
@@ -32,12 +33,12 @@ function CarPage() {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
+        <PopularProfiles mobile />
         <Car {...car.results[0]} setCars={setCar} carPage />
         <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
+        <PopularProfiles />
       </Col>
     </Row>
   );
