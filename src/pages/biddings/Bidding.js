96ad28e6d1cf281c444b9f808aa44fcb4b616335
Mustafa <mustafa_sahinci@hotmail.com>
2022-12-19 14:54:30 +0000

    add edit bidding form

--- src/pages/biddings/Bidding.js
+++ src/pages/biddings/Bidding.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Media } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Avatar from '../../components/Avatar';
@@ -6,6 +6,7 @@ import styles from '../../styles/Bidding.module.css';
 import { MoreDropdown } from '../../components/MoreDropdown';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { axiosRes } from '../../api/axiosDefaults';
+import BiddingEditForm from './BiddingEditForm';
 
 const Bidding = (props) => {
   const {
@@ -19,6 +20,7 @@ const Bidding = (props) => {
     setBiddings,
   } = props;
 
+  const [showEditForm, setShowEditForm] = useState(false);
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
 
@@ -51,10 +53,24 @@ const Bidding = (props) => {
         <Media.Body className="align-self-center ml-2">
           <span className={styles.Owner}>{owner}</span>
           <span className={styles.Date}>{updated_at}</span>
-          <p>{content}</p>
+          {showEditForm ? (
+            <BiddingEditForm
+              id={id}
+              profile_id={profile_id}
+              content={content}
+              profileImage={profile_image}
+              setBiddings={setBiddings}
+              setShowEditForm={setShowEditForm}
+            />
+          ) : (
+            <p>{content}</p>
+          )}
         </Media.Body>
-        {is_owner && (
-          <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
+        {is_owner && !showEditForm && (
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
         )}
       </Media>
     </div>
